9f07c805d42a9a4b186817f59a22d23158115a6b
Suyeon Kang <skang23@wustl.edu>
2016-01-25 17:50:36 -0600

    monday hw

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Suyeon Kang on 1/25/16.
+//  Copyright © 2016 Suyeon Kang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overview: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
